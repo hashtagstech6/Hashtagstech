8534238f68b4575718e427d8a5da78a87e79db6b
Owais Abdullah <mrowaisabdullah@gmail.com>
2026-05-02 10:35:41 +0500

    URL updated

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -67,7 +67,7 @@ export default function Header() {
   const navigation = [
     { name: "HOME", href: "/" },
     { name: "ABOUT", href: "/about" },
-    { name: "SERVICES", href: "/#services" },
+    { name: "SERVICES", href: "/services" },
     { name: "TEAM", href: "/team" },
     { name: "CAREER", href: "/career" },
     { name: "BLOG", href: "/blog" },
